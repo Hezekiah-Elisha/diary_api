04dd140f0cbd5527b3eeb1a860c7be8961f5431b
Hezekiah Elisha <elishahezekiah903@gmail.com>
2025-02-23 22:16:17 +0300

    Add CORS middleware to main application for improved API security

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"diary_api/config"
 	"diary_api/routes"
 	"os"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -19,6 +21,19 @@ func main() {
 	gin.SetMode(ginMode)
 
 	r := gin.Default()
+	r.Use(gin.Logger())
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost"},
+		AllowMethods:     []string{"PUT", "PATCH", "GET", "POST", "DELETE"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return origin == "https://github.com"
+		},
+		MaxAge: 12 * time.Hour,
+	}))
 
 	config.ConnectDB()
 
